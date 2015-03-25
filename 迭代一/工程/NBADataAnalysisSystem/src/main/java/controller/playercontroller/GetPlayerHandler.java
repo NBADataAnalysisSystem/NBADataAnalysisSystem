02c31c3faf13bf0019dae9880fda477bbd0ad504
dalecgu <131250169@smail.nju.edu.cn>
2015-03-25 14:25:36 +0800

    增加了球员的属性；处理了强制类型转换错误

--- 迭代一/工程/NBADataAnalysisSystem/src/main/java/controller/playercontroller/GetPlayerHandler.java
+++ 迭代一/工程/NBADataAnalysisSystem/src/main/java/controller/playercontroller/GetPlayerHandler.java
@@ -2,16 +2,22 @@ package controller.playercontroller;
 
 import service.playerservice.GetPlayerService;
 import serviceimp.playerserviceimp.GetPlayerServiceImp;
+import controller.controller.ErrorResponse;
 import controller.controller.Request;
 import controller.controller.RequestHandler;
 import controller.controller.Response;
 
 public class GetPlayerHandler implements RequestHandler {
 
-	public Response process(Request request) throws Exception {
+	public Response process(Request request) {
+		Response response;
 		GetPlayerService getPlayerService = new GetPlayerServiceImp();
-		Response response = new GetPlayerResponse(
-				getPlayerService.getPlayer(((GetPlayerRequest) request).getColumnList()));
+		try {
+			response = new GetPlayerResponse(
+					getPlayerService.getPlayer(((GetPlayerRequest) request).getColumnList()));
+		} catch (Exception e) {
+			response = new ErrorResponse(request, e);
+		}
 		return response;
 	}
 
--- 迭代一/工程/NBADataAnalysisSystem/src/main/java/dao/playerdao/PlayerTranslation.java
+++ 迭代一/工程/NBADataAnalysisSystem/src/main/java/dao/playerdao/PlayerTranslation.java
@@ -16,27 +16,32 @@ public class PlayerTranslation {
 		map.put(PlayerInfo.AGE, "age");
 		map.put(PlayerInfo.BIRTH, "birth");
 		map.put(PlayerInfo.EXP, "exp");
-		//map.put(PlayerInfo.FILE_PATH, "age");
 		map.put(PlayerInfo.HEIGHT, "height");
 		map.put(PlayerInfo.NAME, "player_name");
-		//map.put(PlayerInfo.NUM_OF_ASSIST, "assists");
-		//map.put(PlayerInfo.NUM_OF_BLOCK_SHOT, "block_shots");
-		//map.put(PlayerInfo.NUM_OF_DEFENSE, "defences");
-		//map.put(PlayerInfo.NUM_OF_ENTRY_FIELD, "age");
-		//map.put(PlayerInfo.NUM_OF_FOUL, "age");
-		//map.put(PlayerInfo.NUM_OF_OFFENSE, "age");
-		//map.put(PlayerInfo.NUM_OF_REBOUND, "age");
-		//map.put(PlayerInfo.NUM_OF_STARTING_FIELD, "age");
-		//map.put(PlayerInfo.NUM_OF_STEAL, "age");
-		//map.put(PlayerInfo.NUM_OF_TURN_OVER, "age");
+		map.put(PlayerInfo.NUM_OF_ASSIST, "assists");
+		map.put(PlayerInfo.NUM_OF_BLOCK_SHOT, "block_shots");
+		map.put(PlayerInfo.NUM_OF_DEFENSE, "defences");
+		map.put(PlayerInfo.NUM_OF_ENTRY_FIELD, "num_of_match");
+		map.put(PlayerInfo.NUM_OF_FOUL, "fouls");
+		map.put(PlayerInfo.NUM_OF_OFFENSE, "offences");
+		map.put(PlayerInfo.NUM_OF_REBOUND, "rebounds");
+		map.put(PlayerInfo.NUM_OF_STARTING_FIELD, "num_of_start");
+		map.put(PlayerInfo.NUM_OF_STEAL, "steals");
+		map.put(PlayerInfo.NUM_OF_TURN_OVER, "turn_overs");
 		map.put(PlayerInfo.NUMBER, "jersey_number");
 		map.put(PlayerInfo.PLAYER_ID, "id");
 		map.put(PlayerInfo.POSITION, "position");
 		map.put(PlayerInfo.SCHOOL, "school");
-		//map.put(PlayerInfo.SCORING, "scoring");
-		//map.put(PlayerInfo.TEAM, "age");
-		//map.put(PlayerInfo.TIME_OF_PRESENCE, "age");
+		map.put(PlayerInfo.SCORING, "score");
+		map.put(PlayerInfo.TEAM, "team");
+		map.put(PlayerInfo.TIME_OF_PRESENCE, "presence_time");
 		map.put(PlayerInfo.WEIGHT, "weight");
+		map.put(PlayerInfo.NUM_OF_SHOOTING, "shootings");
+		map.put(PlayerInfo.NUM_OF_SHOT, "shots");
+		map.put(PlayerInfo.NUM_OF_THREE_POINT_SHOOTING, "three_point_shootings");
+		map.put(PlayerInfo.NUM_OF_THREE_POINT_SHOT, "three_point_shots");
+		map.put(PlayerInfo.NUM_OF_FREE_THROW_SHOOTING, "free_throw_shootings");
+		map.put(PlayerInfo.NUM_OF_FREE_THROW_SHOT, "free_throw_shots");
 	}
 	
 	public String translation(PlayerInfo playerInfo) {
