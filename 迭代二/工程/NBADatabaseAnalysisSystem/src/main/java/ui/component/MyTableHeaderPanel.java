4dcabc33e59e284344876ff48ed6926554cfce97
MimeLyc <382351559@qq.com>
2015-04-12 17:29:39 +0800

    滚动显示。测试

---
+++ 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/ui/component/MyTableHeaderPanel.java
@@ -0,0 +1,39 @@
+package ui.component;
+
+import java.util.ArrayList;
+
+import javax.swing.JLabel;
+
+public class MyTableHeaderPanel extends MyTablePanel{
+
+	JLabel[] header;
+	final static int allRow = 1;
+	final static int pageRow = 1;
+	
+	public MyTableHeaderPanel(int totalRow, int totalColumn, int pageRows,
+			int pageColumn, int width, int height) {
+		super(allRow, totalColumn, pageRow, pageColumn, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setContent(String[] content){
+		
+			for(int j = 0; j < content.length && j < allColumns; j++){
+				allContent[0][j] = content[j];
+			}
+		this.setTableContent();
+	
+	}	
+	
+	protected void setTableContent(){
+			for(int j = 0; j < pageColumn; j++){
+				pageContent[0][j] = allContent[0][j+pointerColumn];
+			}
+		table.setFieldContent(pageContent);
+		table.updateUI();
+	}
+	
+	
+
+
+}
--- 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/ui/component/Test.java
+++ 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/ui/component/Test.java
@@ -1,19 +1,23 @@
 package ui.component;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 
 public class Test{
 	static JPanel panel = new JPanel();
 	static JLabel background = new JLabel();
 	static MyTablePanel testTable;
-	static int high = 1000;
-	static int width = 1000;
+	static MyTableHeaderPanel testHeader;
+	static int high = 700;
+	static int width = 700;
 	public Test(int high,int width){
 
 	}
@@ -35,32 +39,50 @@ public class Test{
 		
 		ImageIcon iconB ;
 		iconB = new ImageIcon("resource/BackgroundOfPlayerChecking.png");
-		iconB.setImage(iconB.getImage().getScaledInstance(1000, 1000,Image.SCALE_DEFAULT));
+		iconB.setImage(iconB.getImage().getScaledInstance(700, 700,Image.SCALE_DEFAULT));
 		
 		
 		JFrame frame = new JFrame();
 		frame.setSize(high, width);
-		testTable = new MyTablePanel(10,10,10,10,high,width);
+		testTable = new MyTablePanel(10,10,10,10,1000,1000);
 		panel.setLayout(null);
 		panel.setSize(high, width);
 		panel.setLocation(0, 0);
 		testTable.setContent(s);
 		testTable.setLocation(0, 0);
+		//TODO 重要，保证testTable大小大于JSCROLLPANE.
+		testTable.setPreferredSize(new Dimension(1000,1000));
 		for(int i = 0; i <10;){
 				testTable.setRowBackground(icon, i++);
 				testTable.setRowBackground(iconT, i++);
 		}
 		
+		testHeader = new MyTableHeaderPanel(1,10,1,10,1000,50);
+		testHeader.setContent(s[0]);
+		//testHeader.setLocation(0, 0);
+		//TODO 重要，保证testTable大小大于JSCROLLPANE.
+		testHeader.setPreferredSize(new Dimension(1000,50));
+		testHeader.setRowBackground(iconT, 0);
+		
+		
 		
 		frame.setLocationRelativeTo(null);
 		frame.add(panel);
 		background.setIcon(iconB);
-		background.setBounds(0,0,1000,1000); //添加图片到frame的第二层 
+		background.setBounds(0,0,700,700); //添加图片到frame的第二层 
 		background.setOpaque(false);
 	//	frame.getRootPane().add(background,new Integer(Integer.MIN_VALUE)); //获取frame的最上层面板为了设置其背景颜色]
-		panel.add(testTable);
+		//panel.add(testTable);
+		JScrollPane scroll = new JScrollPane(testTable);
+		scroll.setColumnHeaderView(testHeader);
+		scroll.setSize(600, 600);
+//		scroll.setViewportView(testTable);
+	    scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		scroll.setOpaque(false);
+		scroll.getViewport().setOpaque(false); 
+		panel.add(scroll);
 		panel.add(background);
-
+		
 		frame.setVisible(true);
 	}
 
