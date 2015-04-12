db8767469cc3bb104a537f6a928d83f4bf7427c1
MimeLyc <382351559@qq.com>
2015-04-12 09:49:35 +0800

    自制表格，滚动条研究ING

---
+++ 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/ui/component/MyTable.java
@@ -0,0 +1,143 @@
+package ui.component;
+
+import java.awt.Color;
+
+import javax.swing.Icon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+/**
+ * 自制表格
+ * @author L.Y.C
+ * @version 2015/4/12
+ */
+
+public class MyTable {
+	//表格，三维数组，前两维为行和列，固定单元格位置，第三为判定内容或图片
+	JLabel[][][] table ;
+	int row,column;
+	//单元格高与宽
+	int cellH,cellW;
+	//表格坐标
+	int x = 0,y = 0;
+	
+	public MyTable(int row,int column){
+		
+		table = new JLabel[row][column][2];
+		this.row = row;
+		this.column = column;
+		
+		//绘制表格
+		for(int i = 0; i < row; i++){
+			for(int j = 0; j < column; j++){
+				table[i][j][0] = new JLabel();
+				table[i][j][1] = new JLabel();
+				table[i][j][0].setHorizontalAlignment(SwingConstants.CENTER);
+				//设置表格为透明，方便显示图片
+				table[i][j][0].setOpaque(false);
+				table[i][j][1].setOpaque(false);
+			}
+		}
+		
+	}
+	//表格内容添加，添加内容为String的二维数组
+	public void setFieldContent(String[][] content){
+		
+		for(int i = 0; i < content.length && i < row; i++){
+			for(int j = 0; j < content[0].length && j < column; j++){
+				table[i][j][0].setText(content[i][j]);
+			}
+		}
+		
+	}
+	//设置单元格高
+	public void setRowHeight(int Rheight){
+		cellH = Rheight;
+		this.resetTableBounds();
+	}
+	//设置单元格宽
+	public void setColumnWidth(int Cwidth){
+		cellW = Cwidth;
+		this.resetTableBounds();
+	}
+	//设置表格整体大小
+	public void setSize(int width, int height){
+		cellH = height / row;
+		cellW = width / column;
+		this.resetTableBounds();
+	}
+	//设置坐标
+	public void setLocation(int x, int y){
+		this.x = x;
+		this.y = y;
+		this.resetTableBounds();
+	}
+	//设置表格坐标、大小
+	private void resetTableBounds(){
+		for(int i = 0; i < row; i++){
+			for(int j = 0; j < column; j++){
+				table[i][j][0].setBounds(x+j*cellW, y+i*cellH, cellW, cellH);
+				table[i][j][1].setBounds(x+j*cellW, y+i*cellH, cellW, cellH);
+			}
+		}
+	}
+	//刷新表格
+	public void updateUI(){
+		for(int i = 0; i < row; i++){
+			for(int j = 0; j < column; j++){
+				table[i][j][0].updateUI();
+				table[i][j][1].updateUI();
+			}
+		}
+	}
+	//将表格添加到Panel中
+	protected void addToPanel(JPanel panel){
+		
+		for(int i = 0; i < row; i++){
+			for(int j = 0; j < column; j++){
+				//先添加背景，后添加内容
+				panel.add(table[i][j][1], 0);
+				panel.add(table[i][j][0], 0);
+			}
+		}
+
+	}
+	//设置字体颜色
+	protected void setFontColor(Color c){
+		for(int i = 0; i < row; i++){
+			for(int j = 0; j < column; j++){
+				table[i][j][0].setForeground(c);
+			}
+		}
+	}
+	//设置某列字体颜色
+	protected void setFontColorColumn(Color c, int column){
+		if(column >= this.column || column < 0) return;
+		for(int i = 0; i < row; i++){
+			table[i][column][0].setForeground(c);
+		}
+	}
+	//设置某行字体颜色
+	protected void setFontColorRow(Color c, int row){
+		if(row >= this.row || row < 0) return;
+		for(int j = 0; j < column; j++){
+			table[row][j][0].setForeground(c);
+		}
+	}
+	//设置某行背景
+	protected void setRowBackground(Icon icon, int row){
+		if(row >= this.row || row < 0) return;
+		for(int j = 0; j < column; j++){
+			table[row][j][1].setIcon(icon);
+		}
+	}
+	//设置某列背景
+	protected void setColumnBackground(Icon icon, int column){
+		if(column >= this.column || column < 0) return;
+		for(int i = 0; i < row; i++){
+			table[i][column][1].setIcon(icon);
+		}
+	}
+	
+}
---
+++ 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/ui/component/MyTablePanel.java
@@ -0,0 +1,161 @@
+package ui.component;
+
+import java.awt.Color;
+
+import javax.swing.Icon;
+import javax.swing.JPanel;
+
+/**
+ * 放置表格的Panel
+ * @author L.Y.C
+ *
+ */
+@SuppressWarnings("serial")
+public class MyTablePanel  extends JPanel{
+
+	MyTable table;
+	//总行、列数
+	int allRows, allColumns;
+	//视图显示行、页数
+	int pageRow, pageColumn;
+	//内容
+	String[][] allContent;
+	String[][] pageContent;
+	//指向目前显示在table上面的最左上角的行和列，指向的列同时是headList的列，相当于指针
+	public int pointerRow = 0;
+	public int pointerColumn = 0;
+	/**
+	 * @param totalRow 总行数
+	 * @param totalColumn 总列数
+	 * @param pageRow 每页行数
+	 * @param pageColumn 每页列数
+	 * @param width 表格宽
+	 * @param height 表格高
+	 */
+	public MyTablePanel(int totalRow, int totalColumn, int pageRow, int pageColumn, int width, int height){
+		
+		this.allRows = totalRow;
+		this.allColumns = totalColumn;
+		this.pageColumn = pageColumn;
+		this.pageRow = pageRow;
+		
+		table = new MyTable(pageRow,pageColumn);
+		table.addToPanel(this);
+		
+		allContent = new String[totalRow][totalColumn];
+		pageContent = new String[pageRow][pageColumn];
+		
+		for(int i = 0; i < totalRow; i++){
+			for(int j = 0; j < totalColumn; j++){
+				allContent[i][j] = new String();
+			}
+		}
+		for(int i = 0; i < pageRow; i++){
+			for(int j = 0; j < pageColumn; j++){
+				pageContent[i][j] = new String();
+			}
+		}
+		
+		this.setTableContent();
+		this.setSize(width, height);
+		this.setTableSize(width, height);
+		this.setTableLocation(0, 0);
+		
+		this.setLayout(null);
+		this.setOpaque(false);
+	}
+	//设置表格内容
+	public void setContent(String[][] content){
+		
+		for(int i = 0; i < content.length && i < allRows; i++){
+			for(int j = 0; j < content[0].length && j < allColumns; j++){
+				allContent[i][j] = content[i][j];
+			}
+		}
+		this.setTableContent();
+	
+	}
+	//设置表格行背景图片
+	public void setRowBackground(Icon icon, int row){
+		table.setRowBackground(icon, row);
+	}
+	
+	public void setColumnBackground(Icon icon, int column){
+		table.setColumnBackground(icon, column);
+	}
+	
+	public void setFontColor(Color c, int row){
+		table.setFontColorRow(c, row);
+	}
+	
+	public void setFontColorColumn(Color c, int column){
+		table.setFontColorColumn(c, column);
+	}
+	
+	public void setFontColor(Color c){
+		table.setFontColor(c);
+	}
+	//设置表格内容
+	protected void setTableContent(){
+		for(int i = 0; i < pageRow; i++){
+			for(int j = 0; j < pageColumn; j++){
+				pageContent[i][j] = allContent[i+pointerRow][j+pointerColumn];
+			}
+		}
+		table.setFieldContent(pageContent);
+		table.updateUI();
+	}
+	//设置表格大小
+	private void setTableSize(int width, int height){
+		table.setSize(width, height);
+	}
+	//设置表格坐标
+	private void setTableLocation(int x, int y){
+		table.setLocation(x, y);
+	}
+	//滚动条相关，表格左右切换
+	public void changeColumn(int change){
+		pointerColumn += change;
+		//设置左右滚动边界
+		if(pointerColumn < 0){
+			pointerColumn = 0;
+		}
+		else if(pointerColumn > allColumns-pageColumn){
+			pointerColumn = allColumns-pageColumn;
+		}
+		this.setTableContent();
+	}
+	//上下滚动条相关
+	public void changeRow(int change){
+		pointerRow += change;
+		if(pointerRow < 0){
+			pointerRow = 0;
+		}
+		else if(pointerRow > allRows-pageRow){
+			pointerRow = allRows-pageRow;
+		}
+		this.setTableContent();
+	}
+	//设置“指针”
+	protected void setTablePointer(int pointerR, int pointerC){
+		pointerRow = pointerR;
+		pointerColumn = pointerC;
+		if(pointerColumn < 0){
+			pointerColumn = 0;
+		}
+		else if(pointerColumn > allColumns-pageColumn){
+			pointerColumn = allColumns-pageColumn;
+		}
+		if(pointerRow < 0){
+			pointerRow = 0;
+		}
+		else if(pointerRow > allRows-pageRow){
+			pointerRow = allRows-pageRow;
+		}
+		this.setTableContent();
+	}
+	
+	
+	
+	
+}
