f84d853040a1a4958565a5d1ab350bde72caf8f6
MimeLyc <382351559@qq.com>
2015-04-14 11:04:57 +0800

    球员查看主要窗体

--- 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/ui/frame/PlayerFrame.java
+++ 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/ui/frame/PlayerFrame.java
@@ -1,11 +1,20 @@
 package ui.frame;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Insets;
+import java.awt.Point;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.BoxLayout;
 import javax.swing.Icon;
@@ -16,11 +25,14 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingUtilities;
 
 import ui.component.MyTableHeaderPanel;
 import ui.component.MyTablePanel;
 
-public class PlayerFrame extends JFrame implements FrameInterface {
+public class PlayerFrame extends JFrame implements FrameInterface, ActionListener {
+
+	private static Point origin = new Point();
 	//设置窗体大小
 	private int height=0;
 	private int width=0;
@@ -57,7 +69,7 @@ public class PlayerFrame extends JFrame implements FrameInterface {
 		//设置Frame原点d
 		x=(Toolkit.getDefaultToolkit().getScreenSize().width-width)/2;
 		y=(Toolkit.getDefaultToolkit().getScreenSize().height-height)/2;
-	//	this.setUndecorated(true);
+		this.setUndecorated(true);
 		this.setSize(width, height);
 		this.setLocation(x,y);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -95,14 +107,98 @@ public class PlayerFrame extends JFrame implements FrameInterface {
 		this.add(backgroundPanel);
 		this.setVisible(true);
 		
+		this. addMouseListener( 
+		        new MouseAdapter(){
+		            public void mousePressed(MouseEvent e){
+		              origin.x = e.getX();
+		              origin.y = e.getY();
+		            }
+
+//		            public void mouseReleased(MouseEvent e) {
+//		              ( (MouseListener) this).mouseReleased(e);
+//		            }
+		            @Override
+		            public void mouseEntered(MouseEvent e) {
+		              repaint();              
+		            }            
+		          }
+		      );
+
+		      this.addMouseMotionListener(
+		          new MouseMotionAdapter(){
+		            public void mouseDragged(MouseEvent e){
+		              Point p =    getLocation();
+		              setLocation(p.x + e.getX() - origin.x, p.y + e.getY() - origin.y );
+		            }          
+		          }
+		      );    
+		
 	}
 	
 	public void setBtnPanel() {
 
 		btnPanel = new JPanel();
+		btnPanel.setLayout(null);
 		btnPanel.setSize(width, height/10);
 		btnPanel.setLocation(0,0);
 		btnPanel.setOpaque(false);
+		
+		JButton btn_Add = new JButton("自定义");
+		btn_Add.setName("add");
+		btn_Add.addActionListener(this);
+
+		btn_Add.setContentAreaFilled(false);
+		btn_Add.setForeground(Color.decode("#FF0000"));
+		btn_Add.setFont(new Font("宋体",1, 20));//设置字体
+		btnPanel.add(btn_Add);
+		btn_Add.setBounds(20,(btnPanel.getHeight())/2-10,100,30);
+		
+		JButton btn_Sift = new JButton("筛  选");
+		btn_Sift.setName("sift");
+		btn_Sift.addActionListener(this);
+
+		btn_Sift.setContentAreaFilled(false);
+		btn_Sift.setForeground(Color.decode("#FF0000"));
+		btn_Sift.setFont(new Font("宋体",1, 20));//设置字体
+		btnPanel.add(btn_Sift);
+		btn_Sift.setBounds(btn_Add.getWidth()+btn_Add.getX()+20,(btnPanel.getHeight())/2-10,100,30);
+		
+		JButton btn_Close = new JButton("×");
+		btn_Close.setMargin(new Insets(0,0,0,0));
+		btn_Close.setBounds(btnPanel.getWidth()-50,(btnPanel.getHeight())/2-10,30, 30);
+		btn_Close.addActionListener(this);
+		btn_Close.setName("close");
+		
+		
+		btn_Close.setContentAreaFilled(false);
+		btn_Close.setForeground(Color.decode("#3A5FCD"));
+		btn_Close.setFont(new Font("Serif",0, 30));//设置字体
+		this.add(btn_Close);
+		
+		JButton btn_Back = new JButton("←");
+		btn_Back.setMargin(new Insets(0,0,0,0));
+		btn_Back.setBounds(btnPanel.getWidth()-60-btn_Close.getWidth(),(btnPanel.getHeight())/2-10,30, 30);
+		btn_Back.addActionListener(this);
+		btn_Back.setName("back");
+		
+		btn_Back.setContentAreaFilled(false);
+		btn_Back.setForeground(Color.decode("#3A5FCD"));
+		btn_Back.setFont(new Font("Serif",0, 20));//设置字体
+		this.add(btn_Back);
+		
+		JButton reduceButton = new JButton("—");
+		reduceButton.setMargin(new Insets(0,0,0,0));
+		reduceButton.setBounds(btnPanel.getWidth()-70-2*btn_Close.getWidth(),(btnPanel.getHeight())/2-10,30,30);
+		reduceButton.addActionListener(new ActionListener(){
+	        public void actionPerformed(ActionEvent e){
+	            setExtendedState(JFrame.ICONIFIED);
+	        }
+	    });
+		
+		reduceButton.setContentAreaFilled(false);
+		reduceButton.setForeground(Color.decode("#3A5FCD"));
+		reduceButton.setFont(new Font("Serif",0, 20));//设置字体
+		this.add(reduceButton);
 
 	}
 	
@@ -168,7 +264,6 @@ public class PlayerFrame extends JFrame implements FrameInterface {
 		sp = new JScrollPane(table);
 		sp.setColumnHeaderView(tableHeaderList);
 		sp.setSize(tableWidth, tableHeight);
-//		scroll.setViewportView(testTable);
 	    sp.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
 		sp.setOpaque(false);
 		sp.getViewport().setOpaque(false); 
@@ -209,6 +304,36 @@ public class PlayerFrame extends JFrame implements FrameInterface {
 	public static void main(String[] args){
 		
 		PlayerFrame test = new PlayerFrame();
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() instanceof JButton){
+			JButton btn = (JButton)e.getSource();
+			String name = btn.getName();
+			if("add".equals(name)){
+				SwingUtilities.invokeLater(new Runnable(){
+					public void run(){
+					//	add();
+					}
+				});
+			}else if("sift".equals(name)){
+				SwingUtilities.invokeLater(new Runnable(){
+					public void run(){
+						//sift();
+					}
+				});
+			}else if("close".equals(name)){
+				  this.setVisible(false);
+				  this.dispose();
+				  System.exit(0);
+			}else if("back".equals(name)){
+				  this.setVisible(false);
+				  this.dispose();
+		//		  @SuppressWarnings("unused")
+			//	FrontFrame front = new FrontFrame();
+			}
+		}
+		
 	}
 
 
