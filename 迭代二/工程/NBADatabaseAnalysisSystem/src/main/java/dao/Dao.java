ff22a61bcd6a37f3eab986e9827ce0f9d810d237
crosseveryriver <1304138563@qq.com>
2015-04-23 20:12:44 +0800

    删除一行

--- 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/dao/Dao.java
+++ 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/dao/Dao.java
@@ -8,7 +8,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
