9be1d00bf1697aa9166058e04571e43f4ddb8061
MimeLyc <382351559@qq.com>
2015-04-28 12:50:56 +0800

    1

--- 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/ui/dlg/PlayerCheckFrame.java
+++ 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/ui/dlg/PlayerCheckFrame.java
@@ -5,9 +5,11 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Insets;
 import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
@@ -19,9 +21,12 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import ui.frame.FrameInterface;
 
 
-public class PlayerCheckFrame extends JFrame {
+public class PlayerCheckFrame extends JFrame implements  ActionListener{
 
 	/**
 	 * 
@@ -36,6 +41,11 @@ public class PlayerCheckFrame extends JFrame {
 	JPanel backgroundPanel;
 	JPanel mainPanel;
 	JPanel basicPanel;
+	JScrollPane sp;
+	JPanel infoPanel;
+	JPanel seasonPanel;
+	JPanel matchPanel;
+	
 
 	
 	public PlayerCheckFrame(){
@@ -75,12 +85,31 @@ public class PlayerCheckFrame extends JFrame {
 			basicPanel.setBounds(0, 0, width, height/3);
 			basicPanel.setOpaque(false);
 			setBasicPanel();
-	
+			
+			infoPanel = new JPanel();
+			infoPanel.setBounds(0, height/3, width, height*2/3);
+			infoPanel.setLayout(new GridLayout(2,1));
+			infoPanel.setOpaque(false);
+			
+			seasonPanel = new JPanel();
+			setSeasonPanel();
+			infoPanel.add(seasonPanel);
+			
+			mainPanel.add(infoPanel);
+			JButton btn_Close = new JButton("×");
+			btn_Close.setMargin(new Insets(0,0,0,0));
+			btn_Close.setBounds(this.getWidth()-30,0,30, 30);
+			btn_Close.addActionListener( this);
+			btn_Close.setContentAreaFilled(false);
+			btn_Close.setForeground(Color.decode("#3A5FCD"));
+			btn_Close.setFont(new Font("Serif",0, 30));//设置字体
+			btn_Close.setName("close");
 		      //TODO
 			mainPanel.add(basicPanel);
 			backgroundPanel.add(placeBackgroundIcon);
 			this.add(backgroundPanel);
 			this.add(mainPanel,0);
+			this.add(btn_Close,0);
 			this.setVisible(true);
 		      
 		  	this. addMouseListener( 
@@ -112,10 +141,20 @@ public class PlayerCheckFrame extends JFrame {
 	}
 
 
+	private void setSeasonPanel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 	@SuppressWarnings("static-access")
 	private void setBasicPanel() {
+		ImageIcon icon = new ImageIcon();
 		basicPanel.setLayout(new GridLayout(1,4));
 		JLabel playerPhoto = new JLabel();
+		icon =  new ImageIcon("resource/BackgroundOfHot.png");
+		icon.setImage(icon.getImage().getScaledInstance(width/4, height/3,Image.SCALE_DEFAULT));
+		playerPhoto.setIcon(icon);
 		basicPanel.add(playerPhoto);
 		JPanel contain;
 		/**
@@ -136,24 +175,24 @@ public class PlayerCheckFrame extends JFrame {
 		JLabel englishName = new JLabel();
 		englishName.setText("ENGLISH");
 		englishName.setFont(new Font("宋体",0,20));
-		englishName.setHorizontalAlignment(number.CENTER);
+		englishName.setHorizontalAlignment(number.LEFT);
 		contain.add(englishName);
 		JLabel chineseName = new JLabel();
 		chineseName.setText("中文-中文中文");
 		chineseName.setFont(new Font("宋体",1,15));
-		chineseName.setHorizontalAlignment(chineseName.CENTER);
+		chineseName.setHorizontalAlignment(chineseName.LEFT);
 		contain.add(chineseName);
 		JLabel teamChinese = new JLabel();
 		teamChinese.setText("中文中文中文");
 		teamChinese.setFont(new Font("宋体",0,15));
 		teamChinese.setForeground(Color.BLUE);
-		teamChinese.setHorizontalAlignment(teamChinese.CENTER);
+		teamChinese.setHorizontalAlignment(teamChinese.LEFT);
 		teamChinese.setVerticalAlignment(teamChinese.BOTTOM);
 		contain.add(teamChinese);
 		JLabel position = new JLabel();
 		position.setText("中文");
-		position.setFont(new Font("宋体",0,15));
-		position.setHorizontalAlignment(position.CENTER);
+		position.setFont(new Font("Courier",0,15));
+		position.setHorizontalAlignment(position.LEFT);
 		position.setVerticalAlignment(position.TOP);
 		contain.add(position);
 		dataPanel.add("Center",contain);
@@ -161,19 +200,69 @@ public class PlayerCheckFrame extends JFrame {
 		 * 设置三个场均信息
 		 */
 		contain = new JPanel();
+		contain.setOpaque(false);
 		contain.setLayout(new GridLayout(2,3));
-		contain.setBorder(BorderFactory.createTitledBorder(""));
-		JLabel temp =new JLabel("?");
-		temp.setFont(new Font("宋体",1,30));
-		contain.add(temp);
+		JLabel temp1 =new JLabel();
+		temp1.setText("场均得分");
+		temp1.setFont(new Font("宋体",0,10));
+		temp1.setForeground(Color.decode("#9B30FF"));
+		contain.add(temp1);
+		JLabel temp2 =new JLabel();
+		temp2.setText("场均篮板");
+		temp2.setFont(new Font("宋体",0,10));
+		temp2.setForeground(Color.decode("#9B30FF"));
+		contain.add(temp2);
+		JLabel temp3 =new JLabel();
+		temp3.setText("场均助攻");
+		temp3.setFont(new Font("宋体",0,10));
+		temp3.setForeground(Color.decode("#9B30FF"));
+		contain.add(temp3);
+		JLabel temp1A =new JLabel();
+		temp1A.setText("11");
+		temp1A.setFont(new Font("宋体",1,20));
+		contain.add(temp1A);
+		JLabel temp2A =new JLabel();
+		temp2A.setText("22");
+		temp2A.setFont(new Font("宋体",1,20));
+		contain.add(temp2A);
+		JLabel temp3A =new JLabel();
+		temp3A.setText("33");
+		temp3A.setFont(new Font("宋体",1,20));
+		contain.add(temp3A);
 		dataPanel.add("South",contain);
 		dataPanel.setOpaque(false);
-		
-		
 		basicPanel.add(dataPanel);
+		/**
+		 * 设置个人信息
+		 */		
 		JPanel basicInfoPanel = new JPanel();
+		basicInfoPanel.setOpaque(false);
+		basicInfoPanel.setLayout(new GridLayout(8,1));
+		basicInfoPanel.add(new JLabel());
+		JLabel whRate = new JLabel();
+		whRate.setText("身高"+"/"+"体重");
+		whRate.setFont(new Font("Serif",0,11));
+		basicInfoPanel.add(whRate);
+		JLabel birthDay = new JLabel();
+		birthDay.setText("生日"+":"+"1111-11-11");
+		birthDay.setFont(new Font("Serif",0,11));
+		basicInfoPanel.add(birthDay);
+		JLabel exp = new JLabel();
+		exp.setText("经历"+":"+"2年");
+		exp.setFont(new Font("Serif",0,11));
+		basicInfoPanel.add(exp);
+		JLabel school = new JLabel();
+		school.setText("加入NBA之前"+":"+"Baylor/United States");
+		school.setFont(new Font("Serif",0,11));
+		basicInfoPanel.add(school);
 		basicPanel.add(basicInfoPanel);
+		
 		JLabel teamIcon = new JLabel();
+		icon =  new ImageIcon("resource/BackgroundOfHot.png");
+		icon.setImage(icon.getImage().getScaledInstance(width/8, height/6,Image.SCALE_DEFAULT));
+		teamIcon.setHorizontalAlignment(teamChinese.RIGHT);
+		teamIcon.setVerticalAlignment(teamChinese.CENTER);
+		teamIcon.setIcon(icon);
 		basicPanel.add(teamIcon);
 		
 		
