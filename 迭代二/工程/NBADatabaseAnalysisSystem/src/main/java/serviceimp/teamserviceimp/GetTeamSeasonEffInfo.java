a772cb0bfeb5bdbc15338c0c56d94f701f168cbe
crosseveryriver <1304138563@qq.com>
2015-04-29 09:35:34 +0800

    提交*2

--- 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/serviceimp/teamserviceimp/GetTeamSeasonEffInfo.java
+++ 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/serviceimp/teamserviceimp/GetTeamSeasonEffInfo.java
@@ -4,20 +4,17 @@ import java.util.ArrayList;
 
 import serviceimp.playerserviceimp.MappingTable;
 import serviceimp.playerserviceimp.PlayerSeasonTotalInfoMappingTable;
-import dao.playerdao.PlayerDao;
-import dao.playerdao.PlayerDaoJdbcImp;
 import dao.teamdao.TeamDao;
 import dao.teamdao.TeamDaoJdbcImp;
 
-public class GetTeamSeasonTotalBasicInfoServiceImp {
-	
-	public ArrayList<String[]> getTeam(String[] sift) {
+public class GetTeamSeasonEffInfo {
+	public ArrayList<String[]> getTeam(String[] sift){
 		TeamDao dao = new TeamDaoJdbcImp();
 		try {
 			MappingTable mappingTable = new PlayerSeasonTotalInfoMappingTable();
 			sift[0] = mappingTable.get(sift[0]);
 			sift[1] = mappingTable.get(sift[1]);
-			return dao.getTeamSeasonTotalBasicInfo(sift);
+			return dao.getTeamSeasonEffInfo(sift);
 		} finally {
 			dao.close();
 		}
