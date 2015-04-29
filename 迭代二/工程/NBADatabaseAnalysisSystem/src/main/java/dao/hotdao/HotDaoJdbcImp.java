f182cd2a03b91c3988d16a786d59a3f112a3fdf1
crosseveryriver <1304138563@qq.com>
2015-04-29 17:48:25 +0800

    赛季热点球员

--- 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/dao/hotdao/HotDaoJdbcImp.java
+++ 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/dao/hotdao/HotDaoJdbcImp.java
@@ -87,7 +87,58 @@ public class HotDaoJdbcImp {
 	
 	private ArrayList<String[]> getSeasonHotPlayerInfo(String sift){
 		ArrayList<String[]> result = new ArrayList<String[]>();
+		Statement stat = null;
+		try {
+			stat = connection.createStatement();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			System.out.println("创建Statement错误！");
+		}
+		ResultSet rs = null;
+		try {
+			rs = stat.executeQuery("select pmp.player_name,pa.path||'players\\portrait\\'||pmp.player_name||'.png', "+
+																"t.full_name,pa.path||'teams\\'||t.full_name||'.png', "+sift+" "+
+													"from matches m, "+
+																"teams t, " +    
+																"paths pa, "+
+																"player_match_performance pmp, "+     
+																"(select max(season) ms "+
+																"from matches) t1, "+
+																"(select pmp.player_id pid ,pmp.team_id ptid "+
+																"from player_match_performance pmp, "+
+																"matches m "+    
+																"where m.id =pmp.match_id and pmp.player_id<>-1 "+
+																"group by pmp.player_id "+ 
+																"having m.season =max(m.season) and m.date_of_match =max(m.date_of_match)) as t2 "+
+													"where m.id=pmp.match_id and m.season = t1.ms and pmp.player_id<>-1 and t2.pid=pmp.player_id and t.id = t2.ptid "+
+													"group by pmp.player_id "+
+													"order by "+sift+" desc;");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("执行Statement 错误！");
+		}
+		int i = 0;
+		try {
+			while (rs.next() && i<5){
+				String[] str = new String[5];
+				for(int j = 0;j < 5;j ++){
+					str[j] = rs.getString(j+1);
+				}
+				result.add(str);
+				i ++;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		for( i = 0 ;i < result.size();i ++){
+			for(int j = 0 ;j < 5;j ++){
+				System.out.print(result.get(i)[j]+" ");
+			}
+			System.out.println();
+			
+		}
 		return result;
 	}
 	
@@ -152,8 +203,8 @@ public class HotDaoJdbcImp {
 		HotDaoJdbcImp t = new HotDaoJdbcImp();
 		String[] strList = new String[2];
 		strList[0] ="13-14";
-		strList[1] = "sum(pmp.shots)";
-		t.getSeasonHotTeamInfo(strList[1]);
+		strList[1] = "round(1.0*sum(pmp.shootings)/count(distinct pmp.match_id),1)";
+		t.getSeasonHotPlayerInfo(strList[1]);
 	}
 
 }
