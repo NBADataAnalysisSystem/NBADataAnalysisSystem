7846c11fd32cffb73f3d4aaadf0eead4721f708b
crosseveryriver <1304138563@qq.com>
2015-04-30 12:26:12 +0800

    去掉多余代码

--- 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/dao/matchdetaildao/MatchDetailDaoJdbcImp.java
+++ 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/dao/matchdetaildao/MatchDetailDaoJdbcImp.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-import dao.hotdao.HotDaoJdbcImp;
+
 
 public class MatchDetailDaoJdbcImp {
 
--- 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/dao/teamdetaildao/TeamDetailDaoJdbcImp.java
+++ 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/dao/teamdetaildao/TeamDetailDaoJdbcImp.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-import dao.matchdetaildao.MatchDetailDaoJdbcImp;
 
 public class TeamDetailDaoJdbcImp {
 	
