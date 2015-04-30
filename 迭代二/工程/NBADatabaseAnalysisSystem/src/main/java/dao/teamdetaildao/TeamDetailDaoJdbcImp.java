9acde6d99eac1e291496c401626ab258c5ec8006
crosseveryriver <1304138563@qq.com>
2015-04-30 12:23:34 +0800

    dao全写完了

--- 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/dao/teamdetaildao/TeamDetailDaoJdbcImp.java
+++ 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/dao/teamdetaildao/TeamDetailDaoJdbcImp.java
@@ -83,10 +83,141 @@ public class TeamDetailDaoJdbcImp {
 	
 	public ArrayList<String[]> getTeamSeasonAvgInfo(String teamFullName){
 		ArrayList<String[]> result = new ArrayList<String[]>();
+		Statement stat = null;
+		try {
+			stat = connection.createStatement();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			System.out.println("创建Statement错误！");
+		}
+		ResultSet rs = null;
+		try {
+			rs = stat.executeQuery("select distinct m.season from matches m");
+			while (rs.next()){
+				String[] strList = new String[14];
+				strList[0] = rs.getString(1);
+				result.add(strList);
+			}
+			for(int i =0;i<result.size();i++){
+			rs = stat.executeQuery("select count(distinct match_id),"
+					+ "round(1.0*sum(pmp.shootings)/sum(pmp.shots),1),"
+					+ "round(1.0*sum(pmp.three_point_shootings)/sum(pmp.three_point_shots),1),"
+					+ "round(1.0*sum(pmp.free_throw_shootings)/sum(pmp.free_throw_shots),1),"
+					+ "round(1.0*sum(pmp.offensive_rebounds)/count(distinct match_id),1),"
+					+ "round(1.0*sum(pmp.defensive_rebounds)/count(distinct match_id),1)"
+					+ ",round(1.0*sum(pmp.rebounds)/count(distinct match_id),1),"
+					+ "round(1.0*sum(pmp.assists)/count(distinct match_id),1),round(1.0*sum(pmp.steals)/count(distinct match_id),1),"
+					+ "round(1.0*sum(pmp.block_shots)/count(distinct match_id),1),round(1.0*sum(pmp.turn_overs)/count(distinct match_id),1),"
+					+ "round(1.0*sum(pmp.fouls)/count(distinct match_id),1),round(1.0*sum(pmp.score)/count(distinct match_id),1) "
+					+ "from matches m,player_match_performance pmp,teams t where m.id=pmp.match_id and "
+					+ "pmp.team_id =t.id and "
+					+ "t.full_name='"+teamFullName+"' and  m.season ='"+result.get(i)[0]+"'");
+			for(int j=1;j<14;j++){
+				result.get(i)[j]=rs.getString(j);
+			}
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		for(int i=0;i<result.size();i++){
+			for(int j=0;j<14;j++){
+				System.out.print(result.get(i)[j]+" ");
+			}
+			System.out.println();
+		}
+		return result;
+	}
+
+	
+	public ArrayList<String[]> getTeamSeasonTotalInfo(String teamFullName){
+		ArrayList<String[]> result = new ArrayList<String[]>();
+		Statement stat = null;
+		try {
+			stat = connection.createStatement();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			System.out.println("创建Statement错误！");
+		}
+		ResultSet rs = null;
+		try {
+			rs = stat.executeQuery("select distinct m.season from matches m");
+			while (rs.next()){
+				String[] strList = new String[17];
+				strList[0] = rs.getString(1);
+				result.add(strList);
+			}
+			for(int i =0;i<result.size();i++){
+			rs = stat.executeQuery("select count(distinct match_id),sum(pmp.shootings),"
+					+ "sum(pmp.shots),sum(pmp.three_point_shootings),sum(pmp.three_point_shots),"
+					+ "sum(pmp.free_throw_shootings),sum(pmp.free_throw_shots),sum(pmp.offensive_rebounds),"
+					+ "sum(pmp.defensive_rebounds),sum(pmp.rebounds),sum(pmp.assists),sum(pmp.steals),"
+					+ "sum(pmp.block_shots),sum(pmp.turn_overs),sum(pmp.fouls),sum(pmp.score) from matches m,player_match_performance pmp,"
+					+ "teams t where m.id=pmp.match_id and pmp.team_id =t.id and "
+					+ "t.full_name='"+teamFullName+"' and  m.season ='"+result.get(i)[0]+"'");
+			for(int j=1;j<17;j++){
+				result.get(i)[j]=rs.getString(j);
+			}
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		for(int i=0;i<result.size();i++){
+			for(int j=0;j<17;j++){
+				System.out.print(result.get(i)[j]+" ");
+			}
+			System.out.println();
+		}
 		
 		return result;
 	}
 
+	public ArrayList<String[]> getTeamPlayerInfo(String teamFullName){
+		ArrayList<String[]> result = new ArrayList<String[]>();
+		Statement stat = null;
+		try {
+			stat = connection.createStatement();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			System.out.println("创建Statement错误！");
+		}
+		ResultSet rs = null;
+		try {
+			rs = stat.executeQuery("select p.player_name,count(distinct pmp.match_id),sum(pmp.is_start),"
+					+ "round(1.0*sum(pmp.presence_time)/60,1),round(1.0*sum(pmp.shootings)/sum(pmp.shots),1),"
+					+ "round(1.0*sum(pmp.three_point_shootings)/sum(pmp.three_point_shots),1),round(1.0*sum(pmp.free_throw_shootings)/sum(pmp.free_throw_shots),1)"
+					+ ",sum(pmp.offensive_rebounds),sum(pmp.defensive_rebounds),sum(pmp.rebounds),round(1.0*sum(pmp.steals)/count(distinct pmp.match_id),1),"
+					+ "round(1.0*sum(pmp.block_shots)/count(distinct pmp.match_id),1),sum(pmp.assists),sum(pmp.fouls),sum(pmp.turn_overs),"
+					+ "round(1.0*sum(pmp.score)/count(distinct pmp.match_id),1) from player_match_performance pmp,teams t,players p "
+					+ "where pmp.player_id=p.id and pmp.team_id =t.id and "
+					+ "t.full_name='"+teamFullName+"' group by p.id");
+			
+			while(rs.next()){
+				String[] strList = new String[16];
+				for(int i = 0 ;i< 16;i++){
+					strList[i]=rs.getString(i+1);
+				}
+				result.add(strList);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		for(int i=0;i<result.size();i++){
+			for(int j=0;j<16;j++){
+				System.out.print(result.get(i)[j]+" ");
+			}
+			System.out.println();
+		}
+		
+		return result;
+	}
 	
 	public void close() {
 		try {
@@ -102,7 +233,7 @@ public class TeamDetailDaoJdbcImp {
 		String[] strList = new String[2];
 		strList[0] ="13-14";
 		strList[1] = "round(1.0*sum(pmp.shootings)/count(distinct pmp.match_id),1)";
-		t.getTeamBasicInfo("Nets");
+		t.getTeamPlayerInfo("Nets");
 	}
 
 }
