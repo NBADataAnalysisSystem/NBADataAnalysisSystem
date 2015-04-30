d330ed9327ef5c0b7246e07aace2643f58fa182b
crosseveryriver <1304138563@qq.com>
2015-04-30 11:14:43 +0800

    map

--- 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/serviceimp/hotserviceimp/HotTeamMappingTable.java
+++ 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/serviceimp/hotserviceimp/HotTeamMappingTable.java
@@ -7,14 +7,14 @@ public class HotTeamMappingTable extends MappingTable {
 	public HotTeamMappingTable() {
 		super();
 		
-		addMap("得分", "");
-		addMap("篮板", "");
-		addMap("助攻", "");
-		addMap("盖帽", "");
-		addMap("抢断", "");
-		addMap("三分%", "");
-		addMap("%", "");
-		addMap("罚球%", "");
+		addMap("得分", "sum(pmp.score)");
+		addMap("篮板", "sum(pmp.rebounds)");
+		addMap("助攻", "sum(pmp.assists)");
+		addMap("盖帽", "sum(pmp.block_shots)");
+		addMap("抢断", "sum(pmp.steals)");
+		addMap("三分%", "round(sum(pmp.three_point_shootings)/sum(pmp.three_point_shots),1)");
+		addMap("%", "round(pmp.shootings)/sum(pmp.shots),1)");
+		addMap("罚球%", "round(pmp.free_throw_shootings)/sum(pmp.free_throw_shots),1)");
 	}
 	
 }
