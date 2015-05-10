9c3bde12618fe2eebde1b446b59a3f90f5d7ec3d
crosseveryriver <1304138563@qq.com>
2015-05-10 23:03:47 +0800

    asd

--- 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/dao/testdao/TestDaoJdbcImp.java
+++ 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/dao/testdao/TestDaoJdbcImp.java
@@ -105,6 +105,87 @@ public class TestDaoJdbcImp {
 		  return result;
 	  }
 
+	  public ArrayList<PlayerNormalInfo> getPlayerAvgNormalInfo(String[] sift){
+		  ArrayList<PlayerNormalInfo> result= new ArrayList<PlayerNormalInfo>();
+		  Statement statement = null;
+			ResultSet resultSet = null;
+			String condition ="";
+			String condition2="";
+			String[] str = sift[4].split(",");
+			for(int i=0;i<str.length;i++){
+				condition2= condition2+str[i].split("\\.")[0]+" "+ str[i].split("\\.")[1]+" ";
+			}
+			if(!sift[1].equals("A")){
+				condition+=" and p.position like '%"+sift[1]+"%' ";
+			}
+			if(!sift[2].equals("A")){
+				condition+=" and t.division like '%"+sift[2]+"%' ";
+			}
+			if(sift[3].equals("1")){
+				condition += " and p.age<=22 ";
+			}else if(sift[3].equals("2")){
+				condition+=" and p.age>22 and p.age<=25 ";
+			}else if(sift[3].equals("3")){
+				condition+=" and p.age>25 and p.age<=30 ";
+			}else if(sift[3].equals("4")){
+				condition+= " and p.age>30 ";
+			}
+			
+			try {
+				statement = connection.createStatement();
+				resultSet = statement.executeQuery("select * from(select  p.player_name name,t.abbreviation teamName,p.age age,round(1.0*sum(pmp.assists)/count(distinct pmp.match_id),1) assist,"+
+"round(1.0*sum(pmp.block_shots)/count(distinct pmp.match_id),1) blockShot,round(1.0*sum(pmp.defensive_rebounds)/count(distinct pmp.match_id),1) defend,"+
+"sum(pmp.score)+sum(pmp.rebounds)+sum(pmp.assists)+sum(pmp.steals)+sum(pmp.block_shots)-(sum(pmp.shots)-sum(pmp.shootings))-"+
+"(sum(pmp.free_throw_shots)-sum(pmp.free_throw_shootings))-sum(pmp.turn_overs) efficiency,round(1.0*sum(pmp.turn_overs)/count(distinct pmp.match_id),1) fault,"+
+"round(1.0*sum(pmp.fouls)/count(distinct pmp.match_id),1) foul,round(1.0*sum(pmp.presence_time)/(60*count(distinct pmp.match_id)),1) minute,"+
+"count(distinct pmp.match_id) numOfGame,round(1.0*sum(pmp.offensive_rebounds)/count(distinct pmp.match_id)) offend,"+
+"round(100.0*sum(pmp.shootings)/sum(pmp.shots),1) penalty,round(1.0*sum(pmp.score)/count(distinct pmp.match_id),1) point,round(1.0*sum(pmp.rebounds)/count(distinct pmp.match_id),1) rebound,"+
+"round(1.0*sum(pmp.shots)/count(distinct pmp.match_id),1) shot,sum(pmp.is_start) start,"+
+"round(1.0*sum(pmp.steals)/count(distinct pmp.match_id),1) steal,round(100.0*sum(pmp.three_point_shootings)/sum(pmp.three_point_shots),1) three "+
+"from players p,"+
+"player_match_performance pmp,"+
+"(select pmp.player_id pid,pmp.team_id tid"+
+" from player_match_performance pmp,"+
+"     matches m     "+
+"where pmp.match_id =m.id and pmp.player_id<>-1 "+
+"group by pmp.player_id "+
+"having m.date_of_match=max(m.date_of_match)) as t1,"+
+"teams t "+
+"where p.id = pmp.player_id and t1.pid =p.id and t.id=t1.tid and  p.position like '%F%' and t.division like '%W%' and p.age>=27 "+
+"group by p.id)"+
+" order by "+condition2+" limit "+sift[0]);
+				while (resultSet.next()) {
+					PlayerNormalInfo pni = new PlayerNormalInfo();
+					pni.setName(resultSet.getString(1));
+					pni.setTeamName(resultSet.getString(2));
+					pni.setAge(resultSet.getInt(3));
+					pni.setAssist(resultSet.getDouble(4));
+					pni.setBlockShot(resultSet.getDouble(5));
+					pni.setDefend(resultSet.getDouble(6));
+					pni.setEfficiency(resultSet.getDouble(7));
+					pni.setFault(resultSet.getDouble(8));
+					pni.setFoul(resultSet.getDouble(9));
+					pni.setMinute(resultSet.getDouble(10));
+					pni.setNumOfGame(resultSet.getInt(11));
+					pni.setOffend(resultSet.getDouble(12));
+					pni.setPenalty(resultSet.getDouble(13));
+					pni.setPoint(resultSet.getDouble(14));
+					pni.setRebound(resultSet.getDouble(15));
+					pni.setShot(resultSet.getDouble(16));
+					pni.setStart(resultSet.getInt(17));
+					pni.setSteal(resultSet.getDouble(18));
+					pni.setThree(resultSet.getDouble(19));
+					result.add(pni);
+				}
+			
+				statement.close();
+			} catch (SQLException e) {
+				System.out.println("SQL错误");
+				e.printStackTrace();
+			}
+		  return result;
+	  }
+	  
 	  public ArrayList<TeamHotInfo> getTeamHotInfo(String[] sift){
 		  ArrayList<TeamHotInfo> result= new ArrayList<TeamHotInfo>();
 		  Statement statement = null;
@@ -157,7 +238,6 @@ public class TestDaoJdbcImp {
 			return result;
 	  }
 
-	  
 	 public ArrayList<TeamNormalInfo> getTeamTotalNormalInfo(String[] sift){
 		 ArrayList<TeamNormalInfo> result= new ArrayList<TeamNormalInfo>();
 		  Statement statement = null;
@@ -204,7 +284,6 @@ public class TestDaoJdbcImp {
 			return result;
 	 } 
 
-	 
 	 public ArrayList<TeamNormalInfo> getTeamAvgNormalInfo(String [] sift){
 		 ArrayList<TeamNormalInfo> result= new ArrayList<TeamNormalInfo>();
 		  Statement statement = null;
