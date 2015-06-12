1c4fdf4618535959d52a34cd8e9c58b61278c4fe
dalecgu <131250169@smail.nju.edu.cn>
2015-06-12 22:54:56 +0800

    xiugai

--- 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/serviceimp/matchserviceimp/GetMatchSectionInfoServiceImp.java
+++ 迭代二/工程/NBADatabaseAnalysisSystem/src/main/java/serviceimp/matchserviceimp/GetMatchSectionInfoServiceImp.java
@@ -2,18 +2,19 @@ package serviceimp.matchserviceimp;
 
 import java.util.ArrayList;
 
-import dao.matchdao.MatchDaoJdbcImp;
+import dao.matchdao.MatchDao;
+import dao.matchdao.MatchDaoJdbcImpV2;
 import service.matchservice.GetMatchSectionInfoService;
 import serviceimp.MappingTable;
 
 public class GetMatchSectionInfoServiceImp implements GetMatchSectionInfoService {
 
 	public ArrayList<String[]> getMatch(String[] sift) {
-		MatchDaoJdbcImp dao = new MatchDaoJdbcImp();
+		MatchDao dao = new MatchDaoJdbcImpV2();
 		try {
 			MappingTable mappingTable = new MatchMappingTable();
 			sift[0] = mappingTable.get(sift[0]);
-			return dao.getMatchSectionInfoV2(sift);
+			return dao.getMatchSectionInfo(sift);
 		} finally {
 			dao.close();
 		}
